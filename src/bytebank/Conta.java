d084e101862e14ed827aac5fc6cf47ff9b2a77c2
unknown <cleber.batista@cruzvita.com>
2022-11-14 17:04:11 -0300

    Criando atributos e instanciando objetos

---
+++ src/bytebank/Conta.java
@@ -0,0 +1,10 @@
+package bytebank;
+
+public class Conta{
+	
+	//atributos, caracteristicas, especificação da conta (Não é um objeto)
+	double saldo;
+	int agencia;
+	int numero;
+	String titular;
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Clebão
+ *
+ */
+module bytebank {
+}
